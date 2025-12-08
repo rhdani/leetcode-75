5bcdd89f76b6dd1e4c7080e472f73877fcb8cbb3
Rahul Dani <rhdani@gmail.com>
2025-12-08 05:42:28 +0000

    Changed file to import TreeNode and BinaryTree
    Updated invertBinaryTree to be more modular now that TreeNode and
    BinaryTree node classes are implemented.
    Also added a driver function so the solution can be tested.

--- invertBinaryTree.py
+++ invertBinaryTree.py
@@ -1,8 +1,5 @@
-class TreeNode:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
+from TreeNode import *
+from BinaryTree import *
 
 def mirror_binary_tree(root):
  
@@ -16,4 +13,34 @@ def mirror_binary_tree(root):
   root.right = root.left
   root.left = tmp
   return root
+
+# Driver code
+def main():
+
+    input_trees = [
+        [TreeNode(100), TreeNode(50), TreeNode(200), TreeNode(25), TreeNode(75), TreeNode(125), TreeNode(350)],
+        [TreeNode(100), TreeNode(50), TreeNode(200), TreeNode(25), TreeNode(110), TreeNode(125), TreeNode(350)],
+        [TreeNode(100), TreeNode(50), TreeNode(200), TreeNode(25), TreeNode(75), TreeNode(90), TreeNode(350)],
+        [TreeNode(25), TreeNode(50), TreeNode(75), TreeNode(100), TreeNode(125), TreeNode(350)],
+        [TreeNode(350), TreeNode(125), TreeNode(100), TreeNode(75), TreeNode(50), TreeNode(25)],
+        [TreeNode(100)],
+        [TreeNode(1), TreeNode(2), None, TreeNode(3), None, TreeNode(4)],
+        [TreeNode(1), TreeNode(2), TreeNode(3), TreeNode(4), None, None, TreeNode(5)],
+        []
+    ]
+
+    indx = 1
+    for i in input_trees:
+        tree = BinaryTree(i)
+
+        print(indx, ".\tBinary Tree:", sep="")
+        indx += 1
+        tree.display_tree()
+        mirror_binary_tree(tree.root)
+        print("\n\tMirrored binary tree:")
+        tree.display_tree()
+        print("-"*100)
+
+if __name__ == '__main__':
+    main()
 
