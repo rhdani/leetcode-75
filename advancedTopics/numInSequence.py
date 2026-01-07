744834d629c058854ac8ab65047296b5d3f686cc
Rahul Dani <rhdani@gmail.com>
2026-01-07 03:19:40 +0000

    updated numIsSeqeunce to use shared union find implementation

--- advancedTopics/numInSequence.py
+++ advancedTopics/numInSequence.py
@@ -3,29 +3,7 @@
 This file implements a UnionFind adapted for a list of unique numbers
 and uses it to compute the longest run of consecutive integers.
 """
-
-class UnionFind:
-    def __init__(self, n):
-        self.parent = list(range(n))
-        self.size = [1] * n
-
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-
-    def union(self, x, y):
-        rx = self.find(x)
-        ry = self.find(y)
-        if rx == ry:
-            return False
-        # union by size
-        if self.size[rx] < self.size[ry]:
-            rx, ry = ry, rx
-        self.parent[ry] = rx
-        self.size[rx] += self.size[ry]
-        return True
-
+from UnionFindSequence import UnionFindSequence as UnionFind
 
 def longest_consecutive_sequence(nums):
     # empty list
