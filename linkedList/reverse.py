2eadb0e269474df4de11f12915faa572840b14a8
Rahul Dani <rhdani@gmail.com>
2026-01-13 20:13:24 +0000

    Added test cases for reverse list

--- linkedList/reverse.py
+++ linkedList/reverse.py
@@ -1,3 +1,7 @@
+from ListNode import *
+from LinkedList import *
+from PrintList import *
+
 def reverse(head):
 
     prev = None
@@ -11,3 +15,26 @@ def reverse(head):
     head = prev
         
     return head
+
+
+def main():
+    input = (
+        [1, 2, 3, 4, 5],
+        [1, 2, 3, 4, 5, 6],
+        [3, 2, 1],
+        [10],
+        [1, 2],
+    )
+
+    for i in range(len(input)):
+        input_linked_list = LinkedList()
+        input_linked_list.create_linked_list(input[i])
+        print(i+1, ".\tInput linked list: ", sep="", end="")
+        print_list_with_forward_arrow(input_linked_list.head)
+        print("\n\tReversed linked list: ", end="")
+        print_list_with_forward_arrow(reverse(input_linked_list.head))
+        print("\n", "-"*100)
+
+
+if __name__ == "__main__":
+    main()
