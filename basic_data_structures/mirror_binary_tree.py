b7cfaddb1383ec9743a61766f9d4f2100089ea4d
Rahul Dani <rhdani@gmail.com>
2026-01-18 23:49:17 +0000

    Solution to mirror a binary tree in place

---
+++ basic_data_structures/mirror_binary_tree.py
@@ -0,0 +1,45 @@
+from BinaryTree import *
+from TreeNode import TreeNode
+
+def mirror_binary_tree(root):
+
+  if root == None:
+    return None
+  if (root.left != None):
+    ltree = mirror_binary_tree(root.left)
+  if (root.right != None):
+    rtree = mirror_binary_tree(root.right)
+
+  root.right, root.left = root.left, root.right
+  return root
+
+# Driver code
+def main():
+    
+    input_trees = [
+        [TreeNode(100), TreeNode(50), TreeNode(200), TreeNode(25), TreeNode(75), TreeNode(125), TreeNode(350)],
+        [TreeNode(100), TreeNode(50), TreeNode(200), TreeNode(25), TreeNode(110), TreeNode(125), TreeNode(350)],
+        [TreeNode(100), TreeNode(50), TreeNode(200), TreeNode(25), TreeNode(75), TreeNode(90), TreeNode(350)],
+        [TreeNode(25), TreeNode(50), TreeNode(75), TreeNode(100), TreeNode(125), TreeNode(350)],
+        [TreeNode(350), TreeNode(125), TreeNode(100), TreeNode(75), TreeNode(50), TreeNode(25)],
+        [TreeNode(100)],
+        [TreeNode(1), TreeNode(2), None, TreeNode(3), None, TreeNode(4)],
+        [TreeNode(1), TreeNode(2), TreeNode(3), TreeNode(4), None, None, TreeNode(5)],
+        []
+    ]
+
+    indx = 1
+    for i in input_trees:
+        tree = BinaryTree(i)
+
+        print(indx, ".\tBinary Tree:", sep="")
+        indx += 1
+        tree.display_tree()
+        mirror_binary_tree(tree.root)
+        print("\n\tMirrored binary tree:")
+        tree.display_tree()
+        #display_tree(tree.root)
+        print("-"*100)
+
+if __name__ == '__main__':
+    main()
