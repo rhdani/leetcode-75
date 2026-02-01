533d002cc317b7fb24e9dcdbcaa6dbfbaede5b4e
Rahul Dani <rhdani@gmail.com>
2026-01-31 21:33:39 -0800

    Solution using interval merging to find free times for all employees given schedules for each employee

---
+++ fusion/employeeFreeTime.py
@@ -0,0 +1,64 @@
+from Interval import Interval
+
+# The input parameter `schedule` is a list of lists, where each inner
+# list contains `Interval` objects representing an employee's schedule.
+def employee_free_time(schedule):  
+    
+    intervals = []
+
+    # Step 1: flatten
+    for employee in schedule:
+        for interval in employee:
+            intervals.append(interval)
+
+    # Step 2: sort by start time
+    intervals.sort(key=lambda x: x.start)
+
+    res = []
+    prev_end = intervals[0].end
+    # Step 3: merge and detect gaps
+    for i in range(1, len(intervals)):
+        curr = intervals[i]
+
+        if curr.start > prev_end:
+            res.append(Interval(prev_end, curr.start))
+
+        prev_end = max(prev_end, curr.end)
+    # Replace this placeholder return statement with your code
+    return res
+    
+# Function for displaying interval list
+def display(vec):
+    string = "["
+    if vec:
+        for i in range(len(vec)):
+            string += str(vec[i])
+            if i + 1 < len(vec):
+                string += ", "
+    string += "]"
+    return string
+
+# Driver code
+def main():
+    inputs = [
+        [[Interval(1, 2), Interval(5, 6)], [Interval(1, 3)], [Interval(4, 10)]],
+        [[Interval(1, 3), Interval(6, 7)], [Interval(2, 4)], [Interval(2, 5), Interval(9, 12)]],
+        [[Interval(2, 3), Interval(7, 9)], [Interval(1, 4), Interval(6, 7)]],
+        [[Interval(3, 5), Interval(8, 10)], [Interval(4, 6), Interval(9, 12)], [Interval(5, 6), Interval(8, 10)]],
+        [[Interval(1, 3), Interval(6, 9), Interval(10, 11)], [Interval(3, 4), Interval(7, 12)], [Interval(1, 3), Interval(7, 10)], [Interval(1, 4)], [Interval(7, 10), Interval(11, 12)]],
+        [[Interval(1, 2), Interval(3, 4), Interval(5, 6), Interval(7, 8)], [Interval(2, 3), Interval(4, 5), Interval(6, 8)]],
+        [[Interval(1, 2), Interval(3, 4), Interval(5, 6), Interval(7, 8), Interval(9, 10), Interval(11, 12)], [Interval(1, 2), Interval(3, 4), Interval(5, 6), Interval(7, 8), Interval(9, 10), Interval(11, 12)], [Interval(1, 2), Interval(3, 4), Interval(5, 6), Interval(7, 8), Interval(9, 10), Interval(11, 12)], [Interval(1, 2), Interval(3, 4), Interval(5, 6), Interval(7, 8), Interval(9, 10), Interval(11, 12)]]
+
+    ]
+    i = 1
+    for schedule in inputs:
+        print(i, '.\tEmployee Schedules:', sep="")
+        for s in schedule:
+            print("\t\t", display(s), sep="")
+        print("\tEmployees' free time", display(employee_free_time(schedule)))
+        print('-'*100)
+        i += 1
+
+
+if __name__ == "__main__":
+    main()
