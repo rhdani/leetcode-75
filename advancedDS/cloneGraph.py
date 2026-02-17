c7d189cda7602a1c410c0e97951778800dbe95fc
Rahul Dani <rhdani@gmail.com>
2026-02-16 21:19:36 -0800

    Updated clone graph to reference common utilities and add test driver

--- advancedDS/cloneGraph.py
+++ advancedDS/cloneGraph.py
@@ -1,7 +1,5 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.neighbors = []
+from Node import *
+from graph_utility import *
 
 def clone(root):
     if root is None:
@@ -16,3 +14,25 @@ def clone(root):
                 stack.append(nei)
             mapping[node].neighbors.append(mapping[nei])
     return mapping[root]
+
+# Driver code
+def main():
+    data = [[[2, 3], [1, 3], [1, 2]],
+            [[2, 4], [1, 3], [2, 4], [1, 3]],
+            [[2, 5], [1, 3], [2, 4], [3, 5], [1, 4]],
+            [[2], [1]],
+            [[2, 6], [1, 3], [2, 4], [3, 5], [4, 6], [1, 5]],
+            [[]]
+            ]
+
+    for i in range (len(data)):
+      node1 = create_graph(data[i])
+      print(i+1, ".\t Original Graph: ", create_2D_list(node1), "\n", sep="")
+      print_graph(node1)
+      print()
+      cloned_root = clone(node1)
+      print("\t Cloned Graph: ", create_2D_list(cloned_root), "\n", sep="")
+      print_graph(node1)
+      print("-"*100)  
+
+main()
